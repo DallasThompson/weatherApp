ee5a553da4898384e9a977d52ef8fc41eaaee738
DallasThompson <duhtennis1@gmail.com>
2023-09-06 20:40:51 -0800

    weatherapp looking pretty good

--- assets/java.js
+++ assets/java.js
@@ -1,18 +1,76 @@
-var buttons = document.querySelectorAll("button");
-var weatherUrl =
-  "https://api.openweathermap.org/data/2.5/forecast?q=Denver,us&appid=cc6f6bc9f9f94c4868709006cb781c83";
+$(function () {
+  var lat;
+  var lon;
+  var currentTemp;
+  var currentWind;
+  var currentHumid;
+  var currentMain;
+  var currentWeatherUrl;
+  var currentWeatherDiv;
+  var city;
 
-for (var i = 0; i < buttons.length; i++) {
-  if (buttons[i].id !== "searchBtn") {
-    buttons[i].classList.add("cityBtn");
-  } else {
-    buttons[i].classList.add("getWeatherBtn");
-  }
-}
-fetch(weatherUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+  ("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=cc6f6bc9f9f94c4868709006cb781c83");
+  $("#searchBtn").on("click", async function () {
+    await getLatLon();
+    await getCurrentWeather();
+    await getWeather();
   });
+
+  async function getCurrentWeather() {
+    var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cc6f6bc9f9f94c4868709006cb781c83`;
+
+    await fetch(currentWeatherUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        $("#currentTemp").text(data.main.temp);
+        $("#currentWind").text(data.main.speed);
+        $("#currentHumid").text(data.main.humidity);
+        $("#currentMain").text(data.weather[0].main);
+        currentTemp = data.main.temp;
+        currentWind = data.wind.speed;
+        currentHumid = data.main.humidity;
+        currentMain = data.weather[0].main;
+      });
+  }
+
+  async function getLatLon() {
+    city = $("#cityInput").val();
+    $("#cityName").text(city);
+    var latLonUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      ",us&appid=cc6f6bc9f9f94c4868709006cb781c83";
+
+    await fetch(latLonUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        lat = data.city.coord.lat;
+        lon = data.city.coord.lon;
+      });
+    console.log("lat", lat);
+  }
+
+  async function getFiveDayForcast() {
+    var weatherUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=cc6f6bc9f9f94c4868709006cb781c83";
+
+    await fetch(weatherUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+});
+// var $div = $("<div>");
+// $div.click(function(){ /* ... */ });
+// $("#box").append($div);
